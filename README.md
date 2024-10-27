7da04f622aecadbe70995de710c2d72c272587a2
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-10-27 11:39:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+	<h1>Colored</h1>
+</div>
+
+<div align="center">
+	<img width="400px" src="./resources/logo.png" alt="">
+</div>
+
+<br>
+
+<div align="center">
+	<img src="https://img.shields.io/visual-studio-marketplace/r/gael-lopes-da-silva.colored?style=for-the-badge&labelColor=000000" alt="">
+	<img src="https://img.shields.io/visual-studio-marketplace/i/gael-lopes-da-silva.colored?style=for-the-badge&labelColor=000000" alt="">
+	<img src="https://img.shields.io/visual-studio-marketplace/d/gael-lopes-da-silva.colored?style=for-the-badge&labelColor=000000" alt="">
+</div>
+
+<div align="center">
+	<a href="./LICENSE.md">
+		<img src="https://img.shields.io/badge/license-BSD%203--Clause-blue?style=for-the-badge&labelColor=000000" alt="">
+	</a>
+</div>
+
+### Colored is a simple extension to highlight colors such as #ffffff and more.
+
+## Options
+#### Todoed has 1 command available right now. `Colored: Toggle Hightlight` that turn on or off the colors highlight.
+
+~~~json
+{
+	"todoed.borderRadius": 3, // Raduis of the colors background
+	"todoed.highlight": true, // Enable or disable highlight of colors
+}
+~~~
+
+## Screenshots
+<img src="./screenshots/colored_1.png" alt="">
+
+## How to build ?
+If you want a build of Colored you can find it in the release section or in the [build](./build/) folder. Else use `vsce package` in the project folder.
+
+## How to install ?
+To install, open visual studio code and go to the extention menu. Click on the three dots and click on `Install from VSIX` and choose the `colored-X.X.X.vsix` file. Or just install it on the market place.
---
+++ src/extension.js
@@ -0,0 +1,151 @@
+// @author: Gael Lopes Da Silva
+// @project: Todoed
+// @github: https://github.com/Gael-Lopes-Da-Silva/TodoedVSCode
+
+const vscode = require('vscode');
+
+let highlight = true;
+let borderRadius = 3;
+let decorationTypes = [];
+
+// ----------------------------------------------------
+
+function activate(context) {
+    loadConfiguration();
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('colored.toggleHighlight', toggleHighlight),
+        vscode.window.onDidChangeActiveTextEditor(onDidChangeActiveTextEditor),
+        vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument),
+        vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration)
+    );
+
+    updateDecorations();
+}
+
+function deactivate() {
+    clearDecorations();
+}
+
+// ----------------------------------------------------
+
+function loadConfiguration() {
+    const config = vscode.workspace.getConfiguration('colored');
+
+    highlight = config.inspect('highlight').globalValue || config.get('highlight');
+    borderRadius = config.inspect('borderRadius').globalValue || config.get('borderRadius');
+}
+
+function toggleHighlight() {
+    highlight = !highlight;
+
+    if (highlight) {
+        updateDecorations();
+    } else {
+        clearDecorations();
+    }
+
+    vscode.window.showInformationMessage(`Colors highlight is now ${highlight ? 'on' : 'off'}.`);
+}
+
+function onDidChangeActiveTextEditor() {
+    updateDecorations();
+}
+
+function onDidChangeTextDocument(event) {
+    if (!vscode.window.activeTextEditor) return;
+
+    if (event.document === vscode.window.activeTextEditor.document) {
+        updateDecorations();
+    }
+}
+
+function onDidChangeConfiguration(event) {
+    if (event.affectsConfiguration('colored')) {
+        clearDecorations();
+        loadConfiguration();
+        updateDecorations();
+    }
+}
+
+function updateDecorations() {
+    if (!highlight) return;
+
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (!activeTextEditor) return;
+
+    const text = activeTextEditor.document.getText();
+    
+    let decorations = new Map();
+    let expresion = "";
+
+    expresion += `#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\\b`;
+    expresion += `|\\brgb\\(\\s*(\\d{1,3})\\s*,\\s*(\\d{1,3})\\s*,\\s*(\\d{1,3})\\s*\\)`;
+
+    const regex = new RegExp(expresion, `g`);
+
+    let match;
+    while ((match = regex.exec(text))) {
+        const startPos = activeTextEditor.document.positionAt(match.index);
+        const endPos = activeTextEditor.document.positionAt(match.index + match[0].length);
+
+        let color = match[0].startsWith('#') ? match[0] : `rgb(${match[2]}, ${match[3]}, ${match[4]})`;
+
+        const isBright = isBrightColor(match[0]);
+        const textColor = isBright ? 'black' : 'white';
+
+        if (!decorations.has(color)) {
+            const decorationType = vscode.window.createTextEditorDecorationType({
+                backgroundColor: color,
+                color: textColor,
+                borderRadius: `${borderRadius}px`
+            });
+            decorations.set(color, {
+                decorationType,
+                ranges: []
+            });
+        }
+
+        decorations.get(color).ranges.push(new vscode.Range(startPos, endPos));
+    }
+
+    clearDecorations();
+
+    for (const { decorationType, ranges } of decorations.values()) {
+        activeTextEditor.setDecorations(decorationType, ranges);
+        decorationTypes.push(decorationType);
+    }
+}
+
+function isBrightColor(color) {
+    let r, g, b;
+
+    if (color.startsWith('#')) {
+        if (color.length === 4) {
+            r = parseInt(color[1] + color[1], 16);
+            g = parseInt(color[2] + color[2], 16);
+            b = parseInt(color[3] + color[3], 16);
+        } else {
+            r = parseInt(color.slice(1, 3), 16);
+            g = parseInt(color.slice(3, 5), 16);
+            b = parseInt(color.slice(5, 7), 16);
+        }
+    } else {
+        r = parseInt(color.match(/\d+/g)[0]);
+        g = parseInt(color.match(/\d+/g)[1]);
+        b = parseInt(color.match(/\d+/g)[2]);
+    }
+
+    const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+    return brightness > 128;
+}
+
+function clearDecorations() {
+    decorationTypes.forEach(decorationType => decorationType.dispose());
+    decorationTypes = [];
+}
+
+module.exports = {
+    activate,
+    deactivate
+};
